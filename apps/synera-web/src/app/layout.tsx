3aab22bdde52ace4ce2e8fa338a792d8f365243f
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2024-03-07 01:38:54 -0300

    feat: add synera logo and delete font exensa

--- apps/synera-web/src/app/layout.tsx
+++ apps/synera-web/src/app/layout.tsx
@@ -1,20 +1,16 @@
 import type { Metadata } from "next"
-import { Rubik, Work_Sans } from "next/font/google"
-import Exensa from "next/font/local"
+import { Work_Sans } from "next/font/google"
 import "./globals.css"
 import Navbar from "@/components/navbar/Navbar"
 
-const rubik = Rubik({ subsets: ["latin"] })
 const workSans = Work_Sans({ subsets: ["latin"] })
-const exensa = Exensa({
-  src: "../font/ExensaGrotesk-Regular.ttf",
-  display: "swap",
-})
 
 export const metadata: Metadata = {
   title: "Synera",
   description:
     "At Synera, we provide high-quality services for the creation or scaling of your projects. Additionally, we specialize in fostering strong client relationships.",
+  keywords:
+    "Synera, software, development, web, mobile, design, consulting, services, projects, client, relationships, agency, company, business, technology, innovation, creativity, quality, experience, expertise, team, professionals, experts, solutions, digital, transformation, growth, success, strategy, development, design, consulting, services, projects, client, relationships, agency, company, business, technology, innovation, creativity, quality, experience, expertise, team, professionals, experts, solutions, digital, transformation, growth, success, strategy",
 }
 
 export default function RootLayout({

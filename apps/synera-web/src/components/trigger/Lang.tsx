650750711f975a8df6bb1e2daf141df7f2fb995d
tomaslami <tomaslamiguralnik@gmail.com>
2024-03-12 11:09:43 -0300

    feat: trigger changes, flags added

--- apps/synera-web/src/components/trigger/Lang.tsx
+++ apps/synera-web/src/components/trigger/Lang.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from "next/image"
 import { usePathname, useRouter } from "next/navigation"
 import React, { useState } from "react"
 
@@ -9,21 +10,36 @@ const Lang = () => {
   const [lang, setlang] = useState(pathname.slice(1, 3))
 
   return (
-    <div className="fixed bottom-5 right-5 w-12 h-12 bg-[#070707] border-[1px] border-white/20 rounded-full cursor-pointer z-50 hover:border-white/30 transition-all">
-      <h6
-        onClick={() => {
-          if (lang === "es") {
-            setlang(pathname.slice(1, 3))
-            router.push("/en")
-          } else {
-            setlang(pathname.slice(1, 3))
-            router.push("/es")
-          }
-        }}
-        className="text-white text-xl w-full h-full flex items-center justify-center select-none"
-      >
-        {lang}
-      </h6>
+    <div className="fixed bottom-5 right-5 w-[46px] h-[46px] bg-[#070707] border-[2px] border-white/20 rounded-full cursor-pointer z-50 hover:border-white/30 transition-all">
+      {
+        lang === "es" ?
+          <span
+            onClick={() => {
+              if (lang === "es") {
+                setlang(pathname.slice(1, 3))
+                router.push("/en")
+              } else {
+                setlang(pathname.slice(1, 3))
+                router.push("/es")
+              }
+            }}
+            className="text-white text-xl w-full h-full flex items-center justify-center select-none"
+          >
+            <Image width={46} height={46} src={"/images/España.svg"} alt={"Logo España"} />
+          </span>
+          :
+          <span onClick={() => {
+            if (lang === "es") {
+              setlang(pathname.slice(1, 3))
+              router.push("/en")
+            } else {
+              setlang(pathname.slice(1, 3))
+              router.push("/es")
+            }
+          }}>
+            <Image width={46} height={46} src={"/images/USA.svg"} alt={"Logo USA"} />
+          </span>
+      }
     </div>
   )
 }

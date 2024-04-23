bf884906ec53b970ee99cf86500cb63cc12ebf40
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2024-04-23 14:10:24 -0300

    feat: delete million

--- apps/synera-web/next.config.mjs
+++ apps/synera-web/next.config.mjs
@@ -1,12 +1,6 @@
-import million from "million/compiler";
- 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
- 
-const millionConfig = {
-  auto: true,
-};
- 
-export default million.next(nextConfig, millionConfig);
+}
+
+export default nextConfig
